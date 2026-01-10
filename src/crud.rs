3f98bf0090c5d7ac8ea277cb3844a1ad1cef75f9
sarkar-segfault <adhritsarkar@proton.me>
2026-01-10 20:31:12 +0530

    refactored create

--- src/crud.rs
+++ src/crud.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 pub type Database = Vec<Task>;
 
-#[derive(Default, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct Task {
     pub id: String,
     pub title: String,
@@ -12,29 +12,26 @@ pub struct Task {
 }
 
 pub fn create(db: &mut Database, id: Option<String>) {
-    let mut task = Task::default();
-
-    task.id = match id {
-        Some(s) => s,
-        None => Input::<String>::new()
-            .with_prompt("enter the id of the new task")
-            .with_initial_text("my_amazing_task")
+    db.push(Task {
+        id: match id {
+            Some(s) => s,
+            None => Input::<String>::new()
+                .with_prompt("enter the id of the new task")
+                .with_initial_text("my_amazing_task")
+                .interact_text()
+                .unwrap_or_else(|e| fatal!("failed to obtain id: {}", e)),
+        },
+        title: Input::<String>::new()
+            .with_prompt("enter the title")
+            .with_initial_text("my amazing task!")
             .interact_text()
-            .unwrap_or_else(|e| fatal!("failed to obtain id: {}", e)),
-    };
-
-    task.title = Input::<String>::new()
-        .with_prompt("enter the title")
-        .with_initial_text("my amazing task!")
-        .interact_text()
-        .unwrap_or_else(|e| fatal!("failed to obtain title: {}", e));
-
-    task.desc = Input::<String>::new()
-        .with_prompt("enter the description")
-        .allow_empty(true)
-        .with_initial_text("this task is so amazing i can't express it in words")
-        .interact_text()
-        .unwrap_or_else(|e| fatal!("failed to obtain description: {}", e));
-
-    db.push(task);
+            .unwrap_or_else(|e| fatal!("failed to obtain title: {}", e)),
+        desc: Input::<String>::new()
+            .with_prompt("enter the description")
+            .allow_empty(true)
+            .with_initial_text("this task is so amazing i can't express it in words")
+            .interact_text()
+            .unwrap_or_else(|e| fatal!("failed to obtain description: {}", e)),
+        status: false,
+    })
 }
